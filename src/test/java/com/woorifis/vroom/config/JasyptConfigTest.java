b0af20e0c7f260a499ddd4239b95c0897cf1f2c9
ghkvud2 <ghkvud2@naver.com>
2022-07-09 00:43:13 +0900

    git ignore prod yml

--- src/test/java/com/woorifis/vroom/config/JasyptConfigTest.java
+++ src/test/java/com/woorifis/vroom/config/JasyptConfigTest.java
@@ -1,4 +1,38 @@
+package com.woorifis.vroom.config;
+
 import static org.junit.jupiter.api.Assertions.*;
-class JasyptConfigTest {
-  
+
+import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@SpringBootTest
+class JasyptApplicationTests {
+
+	@Value("${jasypt.encryptor.password}")
+	private String password;
+
+	@Test
+	void jasypt() {
+		String url = "a";
+		String username = "b";
+		String password = "c";
+
+		log.info("url={}", jasyptEncoding(url));
+		log.info("username={}", jasyptEncoding(username));
+		log.info("password={}", jasyptEncoding(password));
+	}
+
+	public String jasyptEncoding(String value) {
+
+		StandardPBEStringEncryptor pbeEnc = new StandardPBEStringEncryptor();
+		pbeEnc.setAlgorithm("PBEWithMD5AndDES");
+		pbeEnc.setPassword(password);
+		return pbeEnc.encrypt(value);
+	}
+
 }
