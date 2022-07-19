7041c75efc97b6ebfdd76b7adb7ca98648be8fe4
ghkvud2 <leeghkvud@gmail.com>
2022-07-19 10:36:59 +0900

    remove headless

--- src/main/java/com/woorifis/vroom/config/WebDriverConfig.java
+++ src/main/java/com/woorifis/vroom/config/WebDriverConfig.java
@@ -22,7 +22,7 @@ public class WebDriverConfig {
 	public WebDriver chromeDriver() {
 		System.setProperty(DRIVER_NAME, DRIVER_PATH);
 		ChromeOptions options = new ChromeOptions();
-		options.addArguments("headless");
+//		options.addArguments("headless");
 		options.addArguments("start-maximized");
 		options.addArguments("window-size=1920,1080");
 		WebDriver driver = new ChromeDriver(options);
